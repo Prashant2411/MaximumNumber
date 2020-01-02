0e0be7a2c8399a4fd55fe84818d5172ba7ec7a10
Prashant2411 <prashantbedi241197@gmail.com>
2020-01-02 18:18:02 +0530

    Refactored: Created generic class to take three variable of generic type

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -1,5 +1,16 @@
-public class MaximumNumber {
-        public < E extends Comparable > E getMaximum(E a, E b, E c) {
+public class MaximumNumber < E extends Comparable> {
+    E a,b,c;
+
+    public MaximumNumber(E a, E b, E c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public MaximumNumber() {
+    }
+
+    public < E extends Comparable > E getMaximum(E a, E b, E c) {
         E max = a;
         if (b.compareTo(max) > 0) {
             max = b;
@@ -7,6 +18,11 @@ public class MaximumNumber {
         if (c.compareTo(max) > 0) {
             max = c;
         }
+        return max;
+    }
+
+    public E testMaximum() {
+        E max = getMaximum(a, b, c);
         return max;
     }
 }
