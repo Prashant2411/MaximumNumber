505a848a27628a3f2bffbef61ab4e5097ea8c46d
Prashant2411 <prashantbedi241197@gmail.com>
2020-01-02 16:56:46 +0530

    Refactored: Created getMaximum() as Generic for all datatypes

--- src/main/java/MaximumNumber.java
+++ src/main/java/MaximumNumber.java
@@ -1,28 +1,6 @@
 public class MaximumNumber {
-        public Integer getMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-
-    public Float getMaximum(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-
-    public String getMaximum(String a, String b, String c) {
-        String max = a;
+        public < E extends Comparable > E getMaximum(E a, E b, E c) {
+        E max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
